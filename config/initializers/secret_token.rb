54825609bbb4d9edda7b057ef73d04811cda1646
Allison Pearce <alpearce@seas.upenn.edu>
2013-01-10 20:36:50 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cheezburger::Application.config.secret_token = '44b79232a6c3551d69cbd5afc2401d6de5ac372a6fa705ea2491ec520fc823abbc3835e03364339fbc38d2cb0993896c7f48ebd850ac247e6f6552fe4e550d98'
