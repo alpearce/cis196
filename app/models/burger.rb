5639b47390e7ed1356f91c3c2f653dd0108bbaa8
Allison Pearce <alpearce@seas.upenn.edu>
2013-02-12 14:35:40 -0500

    updated new burger form with fancy options

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -1,4 +1,11 @@
 class Burger < ActiveRecord::Base
   belongs_to :order
-  attr_accessible :bun, :cheese, :meat
+  attr_accessible :bun, :cheese, :meat, :order, :order_id
+
+  attr_accessor :new_customer_name
+  before_save :create_order_from_name
+
+  def create_order_from_name
+  	create_order(:customer => new_customer_name) unless new_customer_name.blank?
+  end  
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,4 @@
 class Order < ActiveRecord::Base
   attr_accessible :customer, :price
+  
 end
